1ca5e16ef2f8084b3a52d0c820eb68366514ad62
DurgaPrasad <p,durgaprasad521@gmail.com>
2022-05-05 13:37:22 +0530

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,27 @@
+insert into POS_TBL_Food values('101','Large','pizza',900,2,'nonveg');
+insert into POS_TBL_Food values('102','Small','pizza',250,2,'veg');
+insert into POS_TBL_Food values('103','medium','pizza',400,4,'nonveg');
+insert into POS_TBL_Food values('104','Small','pizza',250,4,'veg');
+insert into POS_TBL_Food values('105','Small','pizza',550,3,'veg');
+ 
+
+insert into POS_TBL_User_Credentials values('111',1,'123456','Admin');
+insert into POS_TBL_User_Credentials values('222',0,'12345786','Cust');
+insert into POS_TBL_User_Credentials values('333',1,'654789','Cust');
+insert into POS_TBL_User_Credentials values('444',0,'12345678','Admin');
+insert into POS_TBL_User_Credentials values('555',1,'91145686','Cust');
+
+
+insert into POS_TBL_Store values('501','Pizzahut','507846','Telangana','Boduppal','Hyd','987456213');
+insert into POS_TBL_Store values('502','Pizzahut','507846','Telangana','Boduppal','Hyd','987456213');
+insert into POS_TBL_Store values('503','Pizzahut','507846','Telangana','Boduppal','Hyd','987456213');
+insert into POS_TBL_Store values('504','Pizzahut','507846','Telangana','Boduppal','Hyd','987456213');
+insert into POS_TBL_Store values('505','Pizzahut','507846','Telangana','Boduppal','Hyd','987456213');
+
+
+insert into POS_TBL_User_Profile values('Hyderabad','p.durgaprasad521@gmail.com','DurgaPrasad','M','Padakanti','Uppal','987456123','500039','Telengana','uppal','111');
+insert into POS_TBL_User_Profile values('Hyderabad','p.durgaprasad521@gmail.com','DurgaPrasad','M','Padakanti','Uppal','987456123','500039','Telengana','uppal','222');
+insert into POS_TBL_User_Profile values('Hyderabad','p.durgaprasad521@gmail.com','DurgaPrasad','M','Padakanti','Uppal','987456123','500039','Telengana','uppal','333');
+insert into POS_TBL_User_Profile values('Hyderabad','p.durgaprasad521@gmail.com','DurgaPrasad','M','Padakanti','Uppal','987456123','500039','Telengana','uppal','444');
+insert into POS_TBL_User_Profile values('Hyderabad','p.durgaprasad521@gmail.com','DurgaPrasad','M','Padakanti','Uppal','987456123','500039','Telengana','uppal','555');
+
---
+++ src/test/java/com/example/demo/Pos1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes= Pos1Application.class)
+class Pos1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
